06a1975ffa8704087e19d3002350f05b13c6e01a
unknown <boaz6151@gmail.com>
2024-11-15 14:00:07 +0200

    Completed

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,32 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String giveme = args[0].toUpperCase();
+                String let = "AEFHILMNORSX";
+                int n = Integer.parseInt(args[1]);
+
+                for (int i = 0; giveme.length() > i; i++) {
+                        char cia = giveme.charAt(i);
+
+                        if (let.indexOf(cia) != -1) {
+
+                                System.out.println("Give me an " + giveme.charAt(i) + ": " + giveme.charAt(i)
+                                                + "!");
+
+                        } else
+
+                        {
+
+                                System.out.println("Give me a " + giveme.charAt(i) + ": " + giveme.charAt(i)
+                                                + "!");
+                        }
+                }
+                        System.out.println("What does that spell?");
+                        for (int j = 0; j < n; j++) {
+                                System.out.println(giveme + "!!!");
+
+                        }
+                
+
         }
 }
