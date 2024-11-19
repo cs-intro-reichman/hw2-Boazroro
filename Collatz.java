d61c842fee7140afca415ddbff0242b3ab7a5b55
unknown <boaz6151@gmail.com>
2024-11-19 11:11:56 +0200

    Completed

--- Collatz.java
+++ Collatz.java
@@ -6,20 +6,20 @@ public class Collatz {
 		int number = Integer.parseInt(args[0]);
 		String letter = args[1];
 		for(int i = 1; i <= number; i++){
-			int x = i;
+			int zmani = i;
 			if(letter.equals("v")){
-				System.out.print(x +" ");
+				System.out.print(zmani +" ");
 			}
 			int counter = 1;
-			while(x != 1 || counter == 1){
-				if(x % 2 == 0){ 
-					x /= 2;
+			while( counter == 1||zmani != 1){
+				if(zmani% 2 == 0){ 
+					zmani /= 2;
 				}
 				else{
-					x = x * 3 + 1;
+					zmani= zmani * 3 + 1;
 				}
 				if(letter.equals("v")){
-					System.out.print(x +" ");
+					System.out.print(zmani +" ");
 				}
 				counter ++;
 			}
@@ -27,7 +27,7 @@ public class Collatz {
 				System.out.println("(" + counter + ")");
 			}		
 		}
-		if (letter.equals("c")) {
+		if (letter.equals("c" )) {
 			System.out.println("Every one of the first " + number + " hailstone sequences reached 1.");
 			
 		}
