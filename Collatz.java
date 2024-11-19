5c06a4a781493547fb564e9ddd3c79f741723de8
unknown <boaz6151@gmail.com>
2024-11-19 10:01:44 +0200

    Completed

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,42 @@
-// Demonstrates the Collatz conjecture.
+// Collatz by Boaz Roro
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+
+		
+		int number = Integer.parseInt(args[0]);
+		String letter = args[1];
+		for(int i = 1; i <= number; i++){
+			int x = i;
+			if(letter.equals("v")){
+				System.out.print(x +" ");
+			}
+			int counter = 1;
+			while(x != 1 || counter == 1){
+				if(x % 2 == 0){ 
+					x /= 2;
+				}
+				else{
+					x = x * 3 + 1;
+				}
+				if(letter.equals("v")){
+					System.out.print(x +" ");
+				}
+				counter ++;
+			}
+			if(letter.equals("v")){	
+				System.out.println("(" + counter + ")");
+			}		
+		}
+		if (letter.equals("c")) {
+			System.out.println("Every one of the first " + number + " hailstone sequences reached 1.");
+			
+		}
+		else
+		{
+		System.out.println("Every one of the first " + number + " hailstone sequences reached 1.");
+		}
 	}
 }
+
+
+
